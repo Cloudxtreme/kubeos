57a511750681a7ff0314c95f3ef10b5fc6e2728b
Kelsey Hightower <kelsey.hightower@gmail.com>
2015-07-05 09:40:12 -0700

    add build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+docker build -t kelseyhightower/kubeos:0.20.2 .
+docker build -t kelseyhightower/etcd:2.0.13 etcd/
+docker build -t kelseyhightower/kube-apiserver:0.20.2 kube-apiserver/
+docker build -t kelseyhightower/kube-controller-manager:0.20.2 kube-controller-manager/
+docker build -t kelseyhightower/kube-proxy:0.20.2 kube-proxy/
+docker build -t kelseyhightower/kube-scheduler:0.20.2 kube-scheduler/
+docker build -t kelseyhightower/kubelet:0.20.2 kubelet/
+docker build -t kelseyhightower/kubestack:0.20.2 kubestack/
